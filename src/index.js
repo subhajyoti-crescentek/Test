6bf46fcff9de2cf487eefc0c95376764144dd105
subhajyotidey <subhajyotidey01@gmail.com>
2020-01-20 12:37:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+
+app.get("/", function (req, res) {
+    res.send("hello world");
+   });
+
+app.listen(3003,()=>{
+    console.log("listening at 3003")
+})
